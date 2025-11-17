0030bcb46d3b380102a1aedc99737cc6c8e6d9de
Heinz Wiesinger <pprkut@liwjatan.org>
2025-11-17 09:27:26 +0100

    Import: Add foundation for content ranges

---
+++ src/Pipeline/Import/Exceptions/ContentRangeException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file contains the Content Range Exception.
+ *
+ * SPDX-FileCopyrightText: Copyright 2025 Framna Netherlands B.V., Zwolle, The Netherlands
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace Pipeline\Import\Exceptions;
+
+use Exception;
+
+/**
+ * Class ContentRangeException.
+ */
+class ContentRangeException extends Exception
+{
+
+}
+
+?>
