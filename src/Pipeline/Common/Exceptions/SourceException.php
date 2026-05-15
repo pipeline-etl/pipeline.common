1175d637b2c066fe702f37dce376f4d40cd0c7ab
Heinz Wiesinger <pprkut@liwjatan.org>
2026-05-15 17:44:01 +0200

    Common: Add an exception for failures when fetching source data

---
+++ src/Pipeline/Common/Exceptions/SourceException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file contains the Source Exception.
+ *
+ * SPDX-FileCopyrightText: Copyright 2026 Framna Netherlands B.V., Zwolle, The Netherlands
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace Pipeline\Common\Exceptions;
+
+use Exception;
+
+/**
+ * Exception for failed source data fetching.
+ */
+class SourceException extends Exception
+{
+
+}
+
+?>
